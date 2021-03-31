297005f14404debc6ad304201bee8022f9375d56
Rene Walliczek <rene.walliczek@mac.com>
2021-04-01 00:21:42 +0200

    Initial Commit

---
+++ snowMan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snowMan
+//
+//  Created by Rene Walliczek on 01.04.21.
+//  Copyright © 2021 Rene Walliczek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
